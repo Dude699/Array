8d26981d63eb8d1721383252964bc2e994b2bbc4
Dude699 <113655625+Dude699@users.noreply.github.com>
2022-12-01 14:38:37 +0700

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -6,14 +6,22 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int a[] = new int[10];
-        int tong = 0;
-        Scanner sc = new Scanner(System.in);
-        for(int i = 0; i <10; i++)  {
-        	a[i] = sc.nextInt();
-        	tong = tong + a[i];
+		 int a[] = new int[10];
+	        int tong = 0;
+	        Scanner sc = new Scanner(System.in);
+	        for(int i = 0; i <10; i++)  {
+	        	a[i] = sc.nextInt();
+	        	tong = tong + a[i];
+        	}
+	        System.out.println("Tong cua cac mang la :" + tong);
+        if(tong / 2 == 0) {
+        	System.out.println("tong la so chan");
+	}else {
+    	System.out.println("tong la so le");
+
         }
-        System.out.println("Tong cua cac mang la :" + tong);
+
 	}
 
 }
+
